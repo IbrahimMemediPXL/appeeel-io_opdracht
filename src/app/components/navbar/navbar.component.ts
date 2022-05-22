3184df71e21c0a1b7ca5330536962241993b8fdc
ibrahimmemedi <ibrahim.memedi@aca-it.be>
2022-05-22 12:18:59 +0200

    implemented header

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,14 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   template: `
-  <p>header</p>
-  `
+    <mat-card>
+      <div class="flex justify-between items-center">
+        <div class="flex items-center">
+          <img src="../assets/GitHub-Mark-32px.png" alt="" />
+          <span class="ml-2 text-lg">Github</span>
+        </div>
+        <div>
+          Appeel.io assessment
+        </div>
+      </div>
+    </mat-card>
+  `,
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
