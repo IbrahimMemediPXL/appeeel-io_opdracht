fb57019051561be6d707dbdc54ab7c75bad3e818
ibrahimmemedi <ibrahim.memedi@aca-it.be>
2022-05-22 11:55:38 +0200

    added navbar component (not implemented)

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  template: `
+  <p>header</p>
+  `
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
